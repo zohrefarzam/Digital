2052d308db2000a55063bd739facaa206f74555b
zohrefarzam <zohrefarzam@gmail.com>
2020-07-10 22:52:52 -0700

    123

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,7 +1,9 @@
 import {combineReducers} from 'redux';
+import {reducer as formReducer} from 'redux-form';
 import prices from './PricesReducer';
 import setting from './SettingReducer';
 export default combineReducers({
   prices,
   setting,
+  form: formReducer,
 });
